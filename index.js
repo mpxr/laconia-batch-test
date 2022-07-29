0b63d0624d2568f8c254bc523e1b41592af3892b
Márton Vető <marton.veto@gmail.com>
2022-07-29 12:16:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const laconiaBatch = require('@laconia/batch');
+const axios = require('axios');
+
+const wait = async (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+const batchJob = (event, context) =>
+
+  laconiaBatch(
+    () =>
+      laconiaBatch.s3({
+        path: '.',
+        s3Params: {
+          Bucket: 'nearst-marton-test-laconia-batch',
+          Key: 'test.json',
+        },
+      }),
+    {
+      itemsPerSecond: 0.5,
+      timeNeededToRecurseInMillis: 10,
+    }
+  )
+    .on('start', () => {
+      console.log(`START`);
+    })
+    .on('item', async (_, item) => {
+      const { data } = await axios.get('https://icanhazdadjoke.com/', {
+        headers: { Accept: 'application/json', 'User-Agent': 'marton-test' },
+      });
+
+      wait(1500);
+
+      const { data: secondJoke } = await axios.get(
+        'https://icanhazdadjoke.com/',
+        {
+          headers: { Accept: 'application/json', 'User-Agent': 'marton-test' },
+        }
+      );
+      console.log('JOKE', {
+        name: item.name,
+        '#1': data.joke,
+        '#2': secondJoke.joke,
+      });
+    })
+    .on('stop', () => {
+      console.log(`STOP`);
+    })
+    .on('end', () => {
+      console.log(`END`);
+    })(event, context);
+
+exports.handler = batchJob;
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Laconia batch test
+
+Test input
+
+```json
+[
+    {
+        "name": "VS Code"
+    },
+    {
+        "name": "Fleet"
+    },
+    {
+        "name": "Eclipse"
+    }
+]
+```
+
+Logs for the first 3 invocations
+
+
+![alt](screenshots/firstinvocation.png)
